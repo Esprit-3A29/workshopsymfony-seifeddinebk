6f596aade6c5439ea6ba394f2d2b59f3578d02b7
seifeddinebk <benkhemisseif@gmail.com>
2022-09-29 01:16:01 +0100

    first commit

---
+++ src/Controller/StudentController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class StudentController extends AbstractController
+{
+    #[Route('/Student', name: 'app_Student')]
+    public function index(): Response
+    {
+        return new Response("Bonjour mes etudiants");
+        
+    }
+}
