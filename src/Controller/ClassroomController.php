5d3f8909e917cd78f6ea882515a1f63b74b3fd0c
seifeddinebk <benkhemisseif@gmail.com>
2022-10-18 18:58:58 +0100

    seif

---
+++ src/Controller/ClassroomController.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Controller;
+use App\Repository\ClassroomRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use App\Entity\Classroom;
+use App\Form\AddClassroomType;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Persistence\ManagerRegistry;
+
+class ClassroomController extends AbstractController
+{
+    #[Route('/classroom', name: 'app_classroom')]
+    public function index(): Response
+    {
+        return $this->render('classroom/index.html.twig', [
+            'controller_name' => 'ClassroomController',
+        ]);
+    }
+
+    
+
+    #[Route('/list', name: 'list_classroom')]
+    public function list(ClassroomRepository $repository):Response
+        {
+    
+            $List=$repository->findAll();
+    
+        return $this->render('classroom/listClassroom.html.twig', [
+         'tab_classroom' => $List,
+     ]);
+        }
+
+    #[Route('/addClassroom', name: 'Add_Classroom')]
+    public function addClassroom(Request $request ,ManagerRegistry $doctrine):Response
+    {
+        $Classroom = new Classroom();
+
+        $form = $this->createForm(AddClassroomType::class, $Classroom);
+        $entityManager = $doctrine->getManager();
+
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+
+            $Classroom = $form->getData();
+
+            // tell Doctrine you want to (eventually) save the Product (no queries yet)
+            $entityManager->persist($Classroom);
+
+            // actually executes the queries (i.e. the INSERT query)
+            $entityManager->flush();
+
+            return $this->redirecttoroute('list_classroom');
+
+        }
+        return $this->render('classroom/AddClassroom.html.twig', [
+            'Form' => $form->createView(),
+
+        ]);
+    }
+
+        #[Route('/updateClassroom/{id}', name: 'Update_Classroom')]
+    public function updateClassroom($id,Request $request ,ManagerRegistry $doctrine ,ClassroomRepository $repository):Response
+        {
+            $Classroom = $repository->find($id);
+
+            $form = $this->createForm(AddClassroomType::class, $Classroom);
+            $entityManager = $doctrine->getManager();
+
+
+            $form->handleRequest($request);
+            if ($form->isSubmitted() && $form->isValid()) {
+
+
+                $Classroom = $form->getData();
+
+                // tell Doctrine you want to (eventually) save the Product (no queries yet)
+                // $entityManager->persist($Classroom);
+
+                // actually executes the queries (i.e. the INSERT query)
+                $entityManager->flush();
+                return $this->redirectToRoute('list_classroom');
+
+
+            }
+            return $this->render('classroom/AddClassroom.html.twig', [
+                'Form' => $form->createView(),
+
+            ]);
+
+        }
+
+
+
+
+
+    #[Route('/deleteClassroom/{id}', name: 'Delete_Classroom')]
+    public function deleteClassroom($id,Request $request ,ManagerRegistry $doctrine ,ClassroomRepository $repository):Response
+    {
+        $Classroom = $repository->find($id);
+
+
+        $entityManager = $doctrine->getManager();
+
+
+        // tell Doctrine you want to (eventually) save the Product (no queries yet)
+        $entityManager->remove($Classroom);
+
+        // actually executes the queries (i.e. the INSERT query)
+        $entityManager->flush();
+        return $this->redirectToRoute('list_classroom');
+
+    }
+
+
+}
--- src/Controller/StudentController.php
+++ src/Controller/StudentController.php
@@ -5,6 +5,12 @@ use App\Repository\StudentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\AddStudentType;
+use Doctrine\Persistence\ManagerRegistry;
+use App\Entity\Student;
+use ContainerGGuUppm\EntityManager_9a5be93;
+use Symfony\Component\HttpFoundation\Request;
+
 
 class StudentController extends AbstractController
 {
@@ -15,10 +21,84 @@ class StudentController extends AbstractController
             'controller_name' => 'StudentController',
         ]);
     }
-    #[Route('/students', name: 'app_student')]
-    public function liststudent(StudentRepository $repository){
-        $students=$repository->findAll();
+    #[Route('/listStudent', name: 'list_student')]
+     public function listStudent(StudentRepository $repository): Response
+    {
 
-        return $this->render("club/listStudent.html.twig",array("tabStudents"=>$students));
+       $List=$repository->findAll();
+
+        return $this->render('student/listStudent.html.twig', [
+            'tab_student' => $List,
+        ]);
     }
+    #[Route('/AddStudent', name: 'Add_Student')]
+    public function AddStudent(Request $request ,ManagerRegistry $doctrine ): Response
+    {
+        $Student = new Student();
+
+        $form = $this->createForm(AddStudentType::class, $Student);
+        $em = $doctrine->getManager();
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+
+            $Student = $form->getData();
+            $em->persist($Student);
+
+
+            $em->flush();
+            return $this->redirecttoroute('list_student');
+        }
+        return $this->render('classroom/AddClassroom.html.twig', [
+            'Form' => $form->createView(),
+
+        ]);
+
+       }
+
+
+       #[Route('/UpdateStudent/{nce}', name: 'Update_Student')]
+
+       public function UpdateStudent($nce,Request $request, ManagerRegistry  $doctrine , StudentRepository $repository):Response
+       {
+   
+   
+           $student = $repository->find($nce);
+           $em = $doctrine->getManager();
+           $form = $this->createForm(AddStudentType::class, $student);
+           $form->handleRequest($request);
+   
+           if ($form->isSubmitted() && $form->isValid()) {
+   
+   
+               $Student = $form->getData();
+   
+   
+               $em->flush();
+               return $this->redirecttoroute('list_student');
+           }
+           return $this->render('student/AddStudent.html.twig', [
+               'Form' => $form->createView(),
+   
+           ]);
+       }
+   
+       #[Route('/DeleteStudent/{nce}', name: 'Delete_Student')]
+   
+   
+       public function DeleteStudent(StudentRepository $repository,$nce,ManagerRegistry $doctrine):Response {
+   
+           $student =$repository->find($nce);
+           $em=$doctrine->getManager();
+           $em->remove($student);
+           $em->flush();
+   
+   
+           return $this->redirectToRoute('list_student');
+   
+       }
+   
+   
+   
+   
 }
